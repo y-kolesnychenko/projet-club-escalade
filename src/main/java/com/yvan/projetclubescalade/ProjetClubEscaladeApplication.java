b015e835328785b8a88217474781d89556e25ac4
Yvan <yvan.kolesnychenko@etu.univ-amu.fr>
2026-03-30 16:23:30 +0200

    init: projet Spring Boot avec dépendances JPA, H2, Security, Mail, JSP

---
+++ src/main/java/com/yvan/projetclubescalade/ProjetClubEscaladeApplication.java
@@ -0,0 +1,13 @@
+package com.yvan.projetclubescalade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetClubEscaladeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetClubEscaladeApplication.class, args);
+    }
+
+}
